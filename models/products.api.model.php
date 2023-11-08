0ee77e6783e77b8e45d78280d8ffe20021fe64b7
Agus Fennema <agusfennema@gmail.com>
2023-11-08 10:57:24 -0300

    arreglado model

--- models/products.api.model.php
+++ models/products.api.model.php
@@ -1,39 +1,71 @@
 <?php
-    class ProductModel{
-        private $db;
 
-    public function __construct {
-        $this->db=new PDO("mysql:host=localhost;"."dbname=tienda;charset=utf8","rout","");
+class productModel {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=tienda;charset=utf8', 'root', '');
     }
 
-    //funcion para obtener todo
-    public funcion getAll{
-        $query=$this->db->prepare("SELECT * from producto");
-        $query->execute;
-        $products = $query->fetchAll(PDO::FETCH_OBJ);
+    /**     * Devuelve la lista de tareas completa.     */
+    public function getAll() {
+        // 1. abro conexión a la DB
+        // ya esta abierta por el constructor de la clase
+        // 2. ejecuto la sentencia (2 subpasos)
+        $query = $this->db->prepare("SELECT * FROM producto");
+        $query->execute();
+        // 3. obtengo los resultados
+        $products = $query->fetchAll(PDO::FETCH_OBJ); // devuelve un arreglo de objetos
         return $products;
     }
 
-    // funcion para obtener productos por id 
-    public funcion getById{
-        $query=$this->db->prepare("SELECT * from producto WHERE ID_producto = ?");
-        $query->execute([$id]);
-        $products = $query->fetch(PDO::FETCH_OBJ);
-        return $products;
+    public function getProductById($ID_producto) {
+        $query = $this->db->prepare("SELECT * FROM producto WHERE ID_producto = ?");
+        $query->execute([$ID_producto]);
+        $productById = $query->fetch(PDO::FETCH_OBJ);
+        
+        return $productById;
     }
 
-    // funcion para obtener ASCENDENTE
-    public funcion orderASC{
-        $query=$this->db->prepare("SELECT * from producto ORDER BY ID_producto ASC");
+    /**     * Inserta una tarea en la base de datos.     */
+    public function insertProduct($ID_categoria_fk, $TIPO, $TALLE, $PRECIO) {
+        $query = $this->db->prepare("INSERT INTO producto (ID_categoria_fk, TIPO, TALLE, PRECIO) VALUES (?, ?, ?, ?)");
+        $query->execute([$ID_categoria_fk, $TIPO, $TALLE, $PRECIO]);
+
+        return $this->db->lastInsertId();
+    }
+
+
+    /**     * Elimina una tarea dado su id.     */
+    function deleteProduct($ID_producto) {
+        $query = $this->db->prepare('DELETE FROM producto WHERE ID_producto = ?');
+        $query->execute([$ID_producto]);
+    }
+
+    function orderASC () {
+        $query = $this->db->prepare("SELECT * FROM producto ORDER BY ID_producto ASC");
         $query->execute();
         $products = $query->fetchAll(PDO::FETCH_OBJ);
         return $products;
+    }
 
-    // funcion para obtener DESCENDENTE
-    public funcion orderDESC{
-        $query=$this->db->prepare("SELECT * from producto ORDER BY ID_producto DESC");
+    function orderDESC () {
+        $query = $this->db->prepare("SELECT * FROM producto ORDER BY ID_producto DESC");
         $query->execute();
         $products = $query->fetchAll(PDO::FETCH_OBJ);
         return $products;
+    }
 
+    function sortbyorder ($sortby = null , $order = null ){
+        $query = $this->db->prepare("SELECT * FROM producto ORDER BY $sortby $order");
+        $query->execute();
+        $products = $query->fetchAll(PDO::FETCH_OBJ);
+        return $products;
+    }
+
+    function update($ID_categoria_fk, $TIPO, $TALLE, $PRECIO, $ID_producto){
+        $query = $this->db->prepare('UPDATE producto SET ID_categoria_fk=?, TIPO=?, TALLE=?, PRECIO=? WHERE ID_producto=?');
+        $query->execute([$ID_categoria_fk, $TIPO, $TALLE, $PRECIO, $ID_producto]);
+    }
 }
