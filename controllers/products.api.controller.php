0057758a3fbb7a3010d479bb69c8e00e5d227caf
Agus Fennema <agusfennema@gmail.com>
2023-11-08 10:52:36 -0300

    actualizado controlador

--- controllers/products.api.controller.php
+++ controllers/products.api.controller.php
@@ -1,44 +1,90 @@
 <?php
-require_once("./models/products.api.model.php");
-require_once("./views/product.api.view.php");
+require_once './app/models/product.model.php';
+require_once './app/views/api.view.php';
 
-class ProductsApiController {
+class productController {
     private $model;
     private $view;
+    private $data;
 
     public function __construct() {
-        $this->model = new ProductsModel();
+        $this->model = new productModel();
         $this->view = new ApiView();
-    }
-
-    public function showProducts($params = null) {
-        $products = $this->model->getProducts();
-            if(isset($_GET["sortby"]) && isset($_GET["order"])) {
-                if($_GET["order" == "ASC"]){
-                    if($_GET["sortby"] == "ID_producto")
-                    $products = $this->model->orderASC();
-                }
-                elseif($_GET["order" == "DESC"]){
-                    if($_GET["sortby"] == "ID_producto")
-                    $products = $this->model->orderDESC();
-                }
-            }
-        else{
-            $products = $this->model->getAll();
-        }
-        return $this->view->response($products,200);
-    }
-
-    // * $params arreglo asociativo con los parámetros del recurso
-    public function showProductsById($params = null) {
-        // obtiene el parametro de la ruta
-        $id = $params[':ID'];
-        $product = $this->model->getById($id);
         
-        if ($tarea) {
-            $this->view->response($product, 200);   
-        } else {
-            $this->view->response("No existe el producto con el id={$ID_producto}", 404);
-        }
+        // lee el body del request
+        $this->data = file_get_contents("php://input");
     }
+
+    // todo lo que llega lo pasa a json
+    private function getData() {
+        return json_decode($this->data);
+    }
+
+    // FUNCION PARA MOSTRAR LOS PRODUCTOS CON ORDEN ASC O DESC
+    public function showAll($params = NULL){
+      $products=$this->model->getAll();
+        if (isset($_GET['sortby']) && isset($_GET['order'])){
+            if($_GET['order'] == 'ASC'){
+                if($_GET['sortby'] == 'id')
+                $products = $this->model->orderASC();//?sortby=id&order=ASC
+                }
+            elseif ($_GET['order'] == 'DESC'){
+                if($_GET['sortby'] == 'id')
+                $products = $this->model->orderDESC();//?sortby=id&order=DESC
+            }
+        }
+        else{
+        $products = $this->model->getAll();
+        }
+        return $this->view->response($products, 200);
+        
+      } 
+
+    // FUNCION PARA MOSTRAR PRODUCTO BY ID
+      public function showProducts($params = NULL) {
+        $ID_producto = $params[':ID'];
+        $products  = $this->model->getProductById($ID_producto);
+        if($products)
+            $this->view->response($products);
+        else 
+            $this->view->response("El producto buscado con el id=$ID_producto no existe", 404);
+      }
+
+      // FUNCION PARA AGREGAR PRODUCTO 
+      public function addProduct($params = NULL){ //añadir un nuevo campeon
+        $productsbyid = $this->getData();  
+        if( empty($productsbyid->ID_categoria_fk) || empty($productsbyid->TIPO)|| empty($productsbyid->TALLE)|| empty($productsbyid->PRECIO)){
+            $this->view->response("Complete los datos", 400);
+        }
+        else {
+            $ID_producto = $this->model->insertProduct($productsbyid->ID_categoria_fk, $productsbyid->TIPO, $productsbyid->TALLE, $productsbyid->PRECIO);
+            $productsbyid = $this->model->getProductById($ID_producto);
+            $this->view->response($productsbyid, 201);
+        }
+      }
+
+      // FUNCION PARA BORRAR PRODUCTO
+      public function deleteProduct($params = NULL) {
+        $ID_producto = $params[':ID'];
+        $products  = $this->model->getProductById($ID_producto);
+      if($products){
+        $this->model->deleteProduct($ID_producto);
+        $this->view->response($products);
+      }
+      else
+      $this->view->response("el producto con el id=$ID_producto no existe", 404);
+        }
+
+    // FUNCION PARA EDITAR/ACTUALIZAR PRODUCTO
+        public function updateProduct($params = null){
+          $ID_producto = $params[':ID'];
+          $product = $this->model->getProductById($ID_producto);
+          if ($product){
+              $product = $this->getData();
+              $this->model->update($product->ID_categoria_fk, $product->TIPO, $product->TALLE, $product->PRECIO, $ID_producto);
+              $this->view->response("el producto con el id=$ID_producto se actualizo correctamente",200);
+              }else {
+              $this->view->response("El producto no existe",404);
+          }
+      }
 }
