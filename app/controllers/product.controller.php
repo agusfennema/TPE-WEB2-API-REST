39e9d3a8856432be9efb0709c6800d7b87912276
Agus Fennema <agusfennema@gmail.com>
2023-11-10 23:22:15 -0300

    changes

--- app/controllers/product.controller.php
+++ app/controllers/product.controller.php
@@ -31,18 +31,18 @@ class productController {
         }
       } 
       else{
-        $products = $this->productModel->getProducts();
+        $products = $this->productModel->getAll();
         return $this->viewApi->response($products, 200);
       } 
     }
 
       public function showProducts($params = NULL) {
         $ID_producto = $params[':ID'];
-        $products  = $this->model->getProductById($ID_producto);
+        $products  = $this->productModel->getProductById($ID_producto);
         if($products)
-            $this->view->response($products, 200);
+            $this->viewApi->response($products, 200);
         else 
-            $this->view->response("El producto buscado con el id=$ID_producto no existe", 404);
+            $this->viewApi->response("El producto buscado con el id=$ID_producto no existe", 404);
       }
 
       public function addProduct($params = NULL){ 
@@ -50,12 +50,12 @@ class productController {
         $productsbyid = $this->getData();  
         
         if( empty($productsbyid->ID_categoria_fk) || empty($productsbyid->TIPO)|| empty($productsbyid->TALLE)|| empty($productsbyid->PRECIO)){
-            $this->view->response("Complete los datos", 400);
+            $this->viewApi->response("Complete los datos", 400);
         }
         else {
-            $ID_producto = $this->model->insertProduct($productsbyid->ID_categoria_fk, $productsbyid->TIPO, $productsbyid->TALLE, $productsbyid->PRECIO);
-            $productsbyid = $this->model->getProductById($ID_producto);
-            $this->view->response("El producto se agregó correctamente", 201);
+            $ID_producto = $this->productModel->insertProduct($productsbyid->ID_categoria_fk, $productsbyid->TIPO, $productsbyid->TALLE, $productsbyid->PRECIO);
+            $productsbyid = $this->productModel->getProductById($ID_producto);
+            $this->viewApi->response("El producto se agregó correctamente", 201);
         }
       }
 
@@ -63,24 +63,24 @@ class productController {
       public function deleteProduct($params = NULL) {
         $ID_producto = $params[':ID'];
       
-        $products  = $this->model->getProductById($ID_producto);
+        $products  = $this->productModel->getProductById($ID_producto);
       if($products){
-        $this->model->deleteProduct($ID_producto);
-        $this->view->response("el producto con el id=$ID_producto se eliminó correctamente", 200);
+        $this->productModel->deleteProduct($ID_producto);
+        $this->viewApi->response("el producto con el id=$ID_producto se eliminó correctamente", 200);
       }
       else
-        $this->view->response("el producto con el id=$ID_producto no existe", 404);
+        $this->viewApi->response("el producto con el id=$ID_producto no existe", 404);
       }
 
         public function updateProduct($params = null){
           $ID_producto = $params[':ID'];
-          $product = $this->model->getProductById($ID_producto);
+          $product = $this->productModel->getProductById($ID_producto);
           if ($product){
               $product = $this->getData();
-              $this->model->update($product->ID_categoria_fk, $product->TIPO, $product->TALLE, $product->PRECIO, $ID_producto);
-              $this->view->response("el producto con el id=$ID_producto se actualizo correctamente",200);
+              $this->productModel->update($product->ID_categoria_fk, $product->TIPO, $product->TALLE, $product->PRECIO, $ID_producto);
+              $this->viewApi->response("el producto con el id=$ID_producto se actualizo correctamente",200);
               }else {
-              $this->view->response("El producto no existe",404);
+              $this->viewApi->response("El producto no existe",404);
           }
       }
 }
